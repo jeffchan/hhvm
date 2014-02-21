31b8f7d5e6d6a71cd59b4055d714971311a2cb9e
Jeff Chan <jefftchan@gmail.com>
2014-02-21 19:55:51 +0000

    Update test runner

--- hphp/test/frameworks/framework_class_overrides/PhpMyAdmin.php
+++ hphp/test/frameworks/framework_class_overrides/PhpMyAdmin.php
@@ -3,6 +3,10 @@ require_once __DIR__.'/../Framework.php';
 
 class PhpMyAdmin extends Framework {
   public function __construct(string $name) {
-    parent::__construct($name, null, null, null, true, TestFindModes::TOKEN);
+    $tc = get_runtime_build().' -v Eval.JitASize=104857600'. // 100MB
+          ' -v Eval.JitAStubsSize=104857600'. // 30MB
+          ' -v Eval.JitGlobalDataSize=31457280'. // 30MB
+          ' '.__DIR__.'/../vendor/bin/phpunit';
+    parent::__construct($name, $tc, null, null, true, TestFindModes::TOKEN);
   }
 }
